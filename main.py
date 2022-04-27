c29f6beee8d1f48e469c1eede61ac6b45f120b67
Dosko987 <doskocilek@gmail.com>
2022-04-27 09:12:18 +0200

    Test

---
+++ main.py
@@ -0,0 +1,29 @@
+from sqlalchemy import Column, String, BigInteger, Integer, DateTime, ForeignKey, Sequence, Table
+from sqlalchemy.orm import relationship
+from sqlalchemy.ext.declarative import declarative_base
+
+BaseModel = declarative_base()
+
+unitedSequence = Sequence('all_id_seq')
+
+class JobModel(BaseModel):
+    __tablename__ = "job"
+
+    id = Column(BigInteger, Sequence('all_id_seq'), primary_key=True)
+    id_teacher = Column(BigInteger)
+    id_study_group = Column(BigInteger)
+    job_name = Column(String)
+    job_topic = Column(String)
+    job_start = Column(DateTime)
+    job_end = Column(DateTime)
+
+class TeacherModel(BaseModel):
+    __tablename__ = "teacher"
+
+    id = Column(BigInteger, Sequence('all_id_seq'), primary_key=True)
+    name = Column(String)
+    surname = Column(String)
+    department = Column(String)
+    mail = Column(String)
+    phone_number = Column(Integer)
+
